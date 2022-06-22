b8a851eb57f4944a8c97ca94fda9c8b6119fa4e9
Steven Burnell <steven.burnell@digital.justice.gov.uk>
2022-06-22 21:50:04 +0100

    Experiment with temporary Google Dashboard settingsconfig.rb

--- config.rb
+++ config.rb
@@ -27,13 +27,15 @@ end
 # Google analytics
 # Analytics ID must be inside the single quotes as it’s string
 configure :development do
-  set :analytics, ""
+#  set :analytics, ""
 #   set :analytics, "UA-XXXXXXXX-X"
+  set :analytics, "UA-162688961-3"
 end
 
 # Insert your Google Analytics ID below
 configure :build do
-  set :analytics, "UA-162688961-1"
+#  set :analytics, "UA-162688961-1"
+  set :analytics, "UA-162688961-3"
 end
 
 ###
