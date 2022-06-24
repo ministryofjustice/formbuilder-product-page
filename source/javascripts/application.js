fa391400a6a2e81db426e59f976d58afcc49c8e9
Steven Burnell <steven.burnell@digital.justice.gov.uk>
2022-06-24 17:05:19 +0100

    Add Cookie Policy banner and controls for Analytics enable/disable

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,2 +1,5 @@
 //= require build/gaap-analytics.min
 //= require start-modules
+//= require mojforms-cookie
+//= require mojforms-analytics
+//= require mojforms-cookie-policy
---
+++ source/javascripts/mojforms-cookie.js
@@ -0,0 +1,31 @@
+var mojforms = mojforms || {};
+
+mojforms.cookie = new (function() {
+
+  this.set = function(name, value, expiry) {
+    var expires = "";
+    if(expiry) {
+      expires = "; expires=" + expiry;
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+  }
+
+  this.get = function(name) {
+    var cookie = null;
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+
+    for(var i=0; i < ca.length; ++i) {
+      var c = ca[i].trim();
+      if(c.indexOf(nameEQ) == 0) {
+        cookie = c.substring(nameEQ.length, c.length);
+      }
+    }
+
+    return cookie;
+  }
+
+  this.remove = function(name) {
+    this.set(name, "", -1);
+  }
+});
